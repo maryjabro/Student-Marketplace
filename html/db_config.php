e26b7edd81344604f26fc0d5a80518b6ccb6aa5a
Mary Jabro <maryjabro@wayne.edu>
2025-12-10 19:37:19 -0500

    Fix DB pass

--- html/db_config.php
+++ html/db_config.php
@@ -4,7 +4,7 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 // Local MySQL using root, no password
 $DB_HOST = "127.0.0.1";
 $DB_USER = "root";
-$DB_PASS = "";  // empty, since you logged in without a password
+$DB_PASS = "";  
 $DB_NAME = "studentMarketplace";
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
